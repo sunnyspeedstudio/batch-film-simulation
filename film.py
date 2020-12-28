d0ca705622f3045b5978489a444da0b0dad84303
sunnyspeedstudio <seesunmoon@gmail.com>
2020-12-27 16:27:30 -0800

    add multiprocessing

--- film.py
+++ film.py
@@ -1,8 +1,9 @@
 import os
 import subprocess
+import multiprocessing
 from PIL import Image, ImageDraw, ImageFont
 
-haldClutFolder = 'C:\\Users\\SUNNY\\Documents\\BOX\\doc\\HaldCLUT'
+haldClutFolder = 'C:\\Users\\SUNNY\\Documents\\HaldCLUT'
 haldClutFileInclusion = '.png'
 haldClutFileExclustion = '(Expired)'
 convertTool = 'convert'
@@ -10,19 +11,30 @@ sampleImageFile = 'sample.jpg'
 outputFolder = 'output'
 
 
+def convert(convertTool, sampleImageFile, haldClutFile, outputFile):
+    print(haldClutFile)
+    print(outputFile)
+
+    process = subprocess.run(
+        [convertTool, sampleImageFile, haldClutFile, '-hald-clut', outputFile], stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE, shell=True)
+    print(process)
+    return
+
+
 # scan through
-for subfolder, folders, files in os.walk(haldClutFolder):
-    for f in files:
-        haldClutFile = subfolder + os.sep + f
-        haldClutName = f[:-4]
+if __name__ == '__main__':
+    pool = multiprocessing.Pool()
 
-        if haldClutFileInclusion in haldClutFile and not haldClutFileExclustion in haldClutFile:
-            outputFile = outputFolder + os.sep + haldClutName + '.jpg'
-            print(haldClutFile)
-            print(haldClutName)
-            print(outputFile)
+    for subfolder, folders, files in os.walk(haldClutFolder):
+        for f in files:
+            haldClutFile = subfolder + os.sep + f
+            haldClutName = f[:-4]
 
-            process = subprocess.run(
-                [convertTool, sampleImageFile, haldClutFile, '-hald-clut', outputFile], stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE, shell=True)
-            print(process)
+            if haldClutFileInclusion in haldClutFile and not haldClutFileExclustion in haldClutFile:
+                outputFile = outputFolder + os.sep + haldClutName + '.jpg'
+
+                pool.apply_async(
+                    convert, args=(convertTool, sampleImageFile, haldClutFile, outputFile, ))
+    pool.close()
+    pool.join()
